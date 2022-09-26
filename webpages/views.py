3ba858a1b78e24f9a4d0cdd8d05b93888fec42ae
Maneesh <maneeshsriram123@gmail.com>
2022-09-26 21:13:19 +0530

    Data visualization

--- webpages/views.py
+++ webpages/views.py
@@ -1,28 +1,25 @@
+from turtle import title
 from django.shortcuts import render
 
 import pandas as pd
 import numpy as np
 import random
-
+import matplotlib.pyplot as plt
+import seaborn as sns
+import plotly.express as px
+import warnings
+warnings.filterwarnings("ignore")
 
 
 def home(request):
     return render(request, 'webpages/home.html')
 
 
+
+
+
 def preprocessing(request):
     return render(request, 'webpages/preprocessing.html')
-def visualization(request):
-    return render(request, 'webpages/visualization.html')
-def model(request):
-    return render(request, 'webpages/model.html')
-def parameter(request):
-    return render(request, 'webpages/parameter.html')
-def prediction(request):
-    return render(request, 'webpages/prediction.html')
-def deployment(request):
-    return render(request, 'webpages/deployment.html')
-
 
 def resPreprocessing(request):
     if request.method == 'POST':
@@ -87,10 +84,128 @@ def resPreprocessing(request):
     return render(request, 'webpages/results/resPreprocessing.html', data)
 
 
+
+
+
+def visualization(request):
+    return render(request, 'webpages/visualization.html')
+
 def resVisualization(request):
-    return render(request, 'webpages/results/resVisualization.html')
+    if request.method == 'POST':
+        file = request.FILES['csvfile']
+        numericaldata = request.POST['num']
+        df = pd.read_csv(file)
+
+        numerical = []
+        for i in numericaldata.split():
+            numerical.append(df.columns[int(i)])
+        
+        col_name = set(df.columns.tolist())
+        num = set(numerical)
+        categorical = col_name - num
+        categorical = list(categorical)
+
+        #Heatmap
+        fig = px.imshow(df.corr(), text_auto=True, title="Heatmap")
+        fig.update_layout(title={'font_size':22, 'xanchor': 'center', 'x':0.5})
+        heat = fig.to_html()
+
+        #Histogram
+        histo = []
+        for i in numerical:
+            try:
+                fig = px.histogram(df[i], nbins=11, labels={'value': i}, title="Histogram")
+                fig.update_layout(bargap=0.2, xaxis=dict(showgrid=False), yaxis=dict(showgrid=False), showlegend=False, title={'font_size':22, 'xanchor': 'center', 'x':0.5})
+                histo.append(fig.to_html())
+            except:
+                continue
+
+        #Pie plot
+        pie = []
+        for i in categorical:
+            try:
+                if(df[i].nunique() < 20):
+                    fig = px.pie(df[i].value_counts(), names=df[i].unique(), title="Pie Plot") 
+                    fig.update_layout(title={'font_size':22, 'xanchor': 'center', 'x':0.5})
+                    pie.append(fig.to_html())
+            except:
+                continue
+
+        #Count plot
+        count = []
+        for i in categorical:
+            try:
+                if(df[i].nunique() < 0.1*len(df)):
+                    fig = px.bar(df[i], title="Count Plot")
+                    fig.update_layout(xaxis=dict(showgrid=False), yaxis=dict(showgrid=False), showlegend=False, title={'font_size': 22, 'xanchor': 'center', 'x': 0.5})
+                    count.append(fig.to_html())
+            except:
+                continue
+
+        #Box plot
+        box = []
+        for i in numerical:
+            try:
+                fig = px.box(df[i], title="Box Plot")
+                fig.update_layout(title={'font_size': 22, 'xanchor': 'center', 'x': 0.5})
+                box.append(fig.to_html())
+            except:
+                continue
+
+        #Line plot
+        line = []
+        for i in numerical:
+            for j in numerical:
+                try:
+                    if(i != j):
+                        fig = px.line(df.sort_values(by=i), x=i, y=j, labels={'x': i, 'y': i}, title="Line Plot")
+                        fig.update_layout(xaxis=dict(showgrid=False), yaxis=dict(showgrid=False), showlegend=False, title={'font_size': 22, 'xanchor': 'center', 'x': 0.5})
+                        line.append(fig.to_html())
+                except:
+                    continue
+
+        #Scatter plot
+        scatter = []
+        for i in numerical:
+            for j in numerical:
+                try:
+                    if(i != j):
+                        fig = px.scatter(df.sort_values(by=i), x=i, y=j, labels={'x': i, 'y': j}, title='Scatter plot')
+                        fig.update_layout(xaxis=dict(showgrid=False), yaxis=dict(showgrid=False), showlegend=False, title={'font_size': 22, 'xanchor': 'center', 'x': 0.5})
+                        scatter.append(fig.to_html())
+                except:
+                    continue
+
+        data = {
+            'heatmap': heat,
+            'histogram': histo,
+            'pie' : pie,
+            'count' : count,
+            'box' : box,
+            'line' : line,
+            'scatter': scatter
+        }
+    return render(request, 'webpages/results/resVisualization.html', data)
 
 
+
+
+
+
+def model(request):
+    return render(request, 'webpages/model.html')
+
+
+def parameter(request):
+    return render(request, 'webpages/parameter.html')
+
+
+def prediction(request):
+    return render(request, 'webpages/prediction.html')
+
+
+def deployment(request):
+    return render(request, 'webpages/deployment.html')
 
 
 
